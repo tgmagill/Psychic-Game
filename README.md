78a17d819cd7a0d855836228fce87cca3e69a7ca
trenton magill <trentonmagill@trentons-MacBook-Pro.local>
2018-08-18 13:15:05 -0700

    Completed Push

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Psychic-Game
-HW JS
+
+Welcome to my Psychic Game where I will challenge you to guess what letter I am thinking of!
+
+I have used Javascript and HTML to create this simple game to reflect my advancing skillset in leveraging these languages.
+
+##Instructions
+
+1. Using your keyboard press a letter that you think I am thinking of.
+1. If you are right the number of wins will increase.  Good Job!
+1. If you are incorrect I will you give you 5 attempts which will count down next to Guesses Left.  After 5 incorrect guesses I will increase the number of losses.
+1. ####Good luck!
---
+++ assets/javascript/game.js
@@ -0,0 +1,77 @@
+var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	
+
+	var wins=0;
+	var losses=0;
+	var guesses=5;
+	var guessesLeft=5;
+	var guessedLetters=[];
+	
+
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	
+
+	// var newGuessesLeft = function () {
+	// 	document.querySelector('#guessLeft').innerHTML = "Guesses Left:" + guessesLeft;
+	// }
+	
+
+	// var newGuessedLetters = function() {
+	// 	document.querySelector('#yourGuessesSoFar').innerHTML = "Your Guesses so far: " + guessedLetters.join(',');
+	// }
+	
+
+	var newGuessesLeft = function () {
+		document.getElementById('guessesLeft').innerHTML = guessesLeft;
+	};
+	
+
+	var newGuessedLetters = function() {
+		document.getElementById('yourGuessesSoFar').innerHTML = guessedLetters.join(',');
+	};
+	
+
+	var reset = function() {
+		computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+	
+
+		guesses = 5;
+		guessesLeft = 5;
+		guessedLetters = [];
+		
+		newGuessesLeft();
+		newGuessedLetters();
+	};
+	
+
+	document.onkeyup = function(event) {
+		guessesLeft--;
+		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		console.log('computerGuess ' + computerGuess);
+		console.log('userGuess ' + userGuess);
+	
+
+		guessedLetters.push(userGuess);	
+		newGuessesLeft();
+		newGuessedLetters();
+	
+
+		if (guessesLeft > 0)
+		{
+			if (userGuess == computerGuess)
+			{
+				wins++;
+				// document.querySelector('#wins').innerHTML = "Wins: " + wins;
+				document.getElementById('wins').innerHTML = wins;
+				reset();
+			}
+		}
+		
+		else if(guessesLeft === 0)
+		{
+			losses++;
+			// document.querySelector('#losses').innerHTML = "Losses: " + losses;
+			document.getElementById('losses').innerHTML = losses;
+			reset();
+		}
+	};
